d6b35afa540e44a70cd40d00d274656e9ac49f46
marekstrelec <marek@mallzee.com>
2016-09-17 10:50:44 +0200

    Server setup

--- backEnd/server.py
+++ backEnd/server.py
@@ -1,21 +1,14 @@
+"""Cloud Foundry test"""
+from flask import Flask
 import os
-try:
-  from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-  from SocketServer import TCPServer as Server
-except ImportError:
-  from http.server import SimpleHTTPRequestHandler as Handler
-  from http.server import HTTPServer as Server
 
-# Read port selected by the cloud for our application
-PORT = int(os.getenv('PORT', 8000))
-# Change current directory to avoid exposure of control files
-os.chdir('static')
+app = Flask(__name__)
+port = int(os.getenv('VCAP_APP_PORT', 8080))
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
 
+@app.route('/')
+def hello_world():
+    return 'Hello World! I am running on port ' + str(port)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)

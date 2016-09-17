9f4142a856d032fb275b93afe535c07c1620f098
Dmytro Khmelenko <d.khmelenko@gmail.com>
2016-09-17 10:06:18 +0200

    Integrate Retrofit for network communication and define calls

--- Android/DocumentsHelper/app/src/main/java/com/hackzurich/documentshelper/network/ServiceApi.java
+++ Android/DocumentsHelper/app/src/main/java/com/hackzurich/documentshelper/network/ServiceApi.java
@@ -1,7 +1,25 @@
 package com.hackzurich.documentshelper.network;
 
+
+import com.hackzurich.documentshelper.model.Document;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import rx.Observable;
+
 /**
  * Define here REST API calls
  */
 public interface ServiceApi {
+
+    @Multipart
+    @POST("/upload")
+    Observable<Document> upload(@Part("description") RequestBody description,
+                                @Part MultipartBody.Part file);
+
 }
